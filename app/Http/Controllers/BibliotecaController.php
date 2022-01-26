212702aea9ab8f6b6df8f5a7828c6264f8dcc807
Luis <luis.gonzalez.gr@usach.cl>
2022-01-25 22:55:37 -0300

    [Luis González]- Controlador Biblioteca con validaciones.

--- app/Http/Controllers/BibliotecaController.php
+++ app/Http/Controllers/BibliotecaController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Libros;
+use App\Models\Usuario;
+use Illuminate\Support\Facades\Validator;
 
 class BibliotecaController extends Controller
 {
@@ -13,7 +16,16 @@ class BibliotecaController extends Controller
      */
     public function index()
     {
-        //
+        $bibliotecas = Biblioteca::all();
+       
+        $libros= array();
+        $libro= Libros::all()->where('delete',FALSE);
+        foreach($bibliotecas as $biblioteca){
+            $libro = Libros::find($biblioteca->idLibro);
+            
+            array_push($libros,$libro);
+        }
+        //return view('biblioteca',compact('libros'));
     }
 
     /**
@@ -34,7 +46,41 @@ class BibliotecaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make(
+            [
+                'idUsuario' => $request->idUsuario,
+                'idLibro' => $request->idLibro,  
+            ],
+            [
+                'idUsuario' => 'required',
+                'idLibro' => 'required',
+            ]
+        );
+        if ($validator->fails())
+        {
+            return response()->json([
+                "message" => 'Los datos ingresados son invalidos.'
+            ]);
+        }
+        $libro = Libros::find($request->idLibro);
+        if($juego == NULL){
+            return response()->json([
+                "message" => 'Id de libro invalido'
+            ]);
+        }
+        $biblioteca = new Biblioteca();
+        $biblioteca->idUsuario = $request->idUsuario;
+        $biblioteca->idLibro = $request->idLibro;
+        $biblioteca->save();
+
+        if ($biblioteca != NULL) {
+            return response()->json([
+                "message" => 'Se ha creado una biblioteca.'
+            ],202);
+        }
+        return response()->json([
+            "message" => 'No se ha creado una biblioteca.'
+        ]);
     }
 
     /**
@@ -45,7 +91,13 @@ class BibliotecaController extends Controller
      */
     public function show($id)
     {
-        //
+        $biblioteca = Biblioteca::find($id);
+        if ($biblioteca != NULL) {
+            return response()->json($biblioteca);
+        }
+        return response()->json([
+            "message" => 'No se encontro ninguna biblioteca con ese id.'
+        ]);
     }
 
     /**
@@ -68,7 +120,52 @@ class BibliotecaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make(
+            [
+                'idUsuario' => $request->idUsuario,
+                'idLibro' => $request->idLibro,
+            ],
+            [
+                'idUsuario' => 'required',
+                'idLibro' => 'required',
+            ]
+        );
+
+        if ($validator->fails())
+        {
+            return response()->json([
+                "message" => 'Los datos ingresados son invalidos.'
+            ]);
+        }
+        $usuario = Usuario::find($request->idUsuario);
+        if($usuario == NULL){
+            return response()->json([
+                "message" => 'Id de usuario invalido.'
+            ]);
+        }
+
+        $libro = Libros::find($request->idlibro);
+        if($juego == NULL){
+            return response()->json([
+                "message" => 'Id de libro invalido.'
+            ]);
+        }
+        
+        $biblioteca = Biblioteca::find($id);
+        if($biblioteca == NULL){
+            return response()->json([
+                "message" => 'El Id es invalido.'
+            ]);
+        }
+
+        if ($request->idUsuario != NULL) {
+            $biblioteca->idUsuario = $request->idUsuario;
+        }
+        if ($request->idlibro != NULL) {
+            $biblioteca->idlibro = $request->idlibro;
+        }
+        $biblioteca->save();
+        return response()->json($biblioteca);
     }
 
     /**
